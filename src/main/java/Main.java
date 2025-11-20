05b740366d724971251f2d64cf8b3f4bc11b90f9
smilo <Valenok730@gmail.com>
2025-11-20 10:07:43 +0300

    Выполненое задание 19.11

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,52 +3,61 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
+     final static int MAX_SPEED = 250;
+    final static int MIN_SPEED = 0;
+
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Car> cars = new ArrayList<>();
 
         System.out.println("///////////////////////////////////");
         System.out.println("Приветсвуем на гонке 24 часа Леман!");
         System.out.println("///////////////////////////////////");
+
+        Race race = new Race();
+        ArrayList<String> result = race.goRacing(readAndWriteCars());
+
+        System.out.println("///////ФИНИШ!///////");
+        for(String i : result){
+            System.out.println(i);
+        }
+    }
+
+     static ArrayList<Car> readAndWriteCars(){
+
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Car> cars = new ArrayList<>();
 
         for (int i = 1;i<=3;i++) {
 
             while (true) {
                 System.out.println("Введите название автомобиля №" + i );
 
-                    String name = scanner.next().trim();
-                    if(name.length()>1) {
+                String name = scanner.next().trim();
+                if(name.length()>1) {
 
-                        while (true) {
-                            System.out.println("Введите скорость автомобиля №" + i + " "+name );
-                            scanner.nextLine();
+                    while (true) {
+                        System.out.println("Введите скорость автомобиля №" + i + " "+name );
+                        scanner.nextLine();
 
-                            if (scanner.hasNextInt()) {
-                                int speed = scanner.nextInt();
+                        if (scanner.hasNextInt()) {
+                            int speed = scanner.nextInt();
 
-                                if (speed >= 0 && speed <= 250) {
-                                    cars.add(new Car(name, speed));
-                                    break;
-                                } else {
-                                    System.out.println("Скорость должна быть от 1 до 250 км/ч,");
-                                }
+                            if (speed >= MIN_SPEED && speed <= MAX_SPEED) {
+                                cars.add(new Car(name, speed));
+                                break;
                             } else {
-                                System.out.println("Нужно ввести скорость целым числом");
+                                System.out.println("Скорость должна быть от 1 до 250 км/ч,");
                             }
+                        } else {
+                            System.out.println("Нужно ввести скорость целым числом");
                         }
-                        break;
-                    }else{
-                        System.out.println("Название должно состоять из двух и более символов");
                     }
+                    break;
+                }else{
+                    System.out.println("Название должно состоять из двух и более символов");
+                }
             }
         }
-        Race race = new Race();
-        ArrayList<String> result = race.goRacing(cars);
-
-        System.out.println("///////ФИНИШ!///////");
-        for(String i : result){
-            System.out.println(i);
-        }
+        return cars;
     }
 }
